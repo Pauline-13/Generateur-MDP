9dfff99bd5540143cc5130552942a972962a7159
Pauline-13 <pauline.tincq@yahoo.fr>
2025-04-24 16:16:09 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,49 @@
+/* ---------------------- VARIABLES -------------------------- */
+let majuscules = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+let minuscules = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+let chiffres = ["0","1","2","3","4","5","6","7","8","9"];
+
+let symboles = [".",",",";",":","!","?","/","-","*","%","µ","£","$","^","=","+","(",")","{","[","]","}","@","#","~","&","²"];
+
+
+
+/* ---------------------- FONCTIONS -------------------------- */
+function genererMdp() {
+    // Récupére le nombre de caractères souhaités ↓
+    const nombreCaracteres = parseInt(document.getElementById('nombre-voulu').value);
+
+    // Vérifie si le nombre est valide (entre 12 et 128) ↓
+    if (nombreCaracteres < 12 || nombreCaracteres > 128) {
+        alert("Entre un nombre entre 12 et 128");
+        return; }
+
+    // Combine tous les caractères en une seule variable ↓
+    let totalCaracteres = [];
+    totalCaracteres = totalCaracteres.concat(majuscules,minuscules,chiffres,symboles);
+
+    // Stock le mot de passe généré ↓
+    let motDePasse = "";
+
+    // Génére un MDP en ajoutant un caractère aléaoire jusqu'à longueur souhaitée ↓
+    for (let i=0; i < nombreCaracteres; i++) {
+        // sélectionne un caractère aléatoire du tableau `totalCaracteres` ↓
+        const caractereAleatoire = totalCaracteres[Math.floor(Math.random() * totalCaracteres.length)];
+        // Ajoute le caractère au MDP ↓
+        motDePasse += caractereAleatoire;
+    } 
+
+    // Affiche le MDP dans la balise <p> ↓
+    document.getElementById('resultat-mdp').textContent = motDePasse;
+}
+
+
+
+
+
+
+
+
+
+
